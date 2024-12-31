bcd82f6102c03622d2c83666ab18e445c9b650ff
AmiraAdel28 <amira.adel282001@gmail.com>
2024-12-31 21:57:06 +0200

    Encapsulation

---
+++ Assignment2_Session2_OOP/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_Session2_OOP
+{
+    internal struct Employee
+    {
+        //Attributes Will Private
+         int Id;
+         string Name;
+         decimal salary;
+        public int Age { get; set; } //Automatic Property
+
+        //2.1 Encapsulation With Getter Setter
+        //Getter
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string _name)
+        {
+             Name =_name ;
+        }
+
+        //2.2 Encapsulation With Property
+
+        // Full Property
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+
+
+        public Employee(int _id  , string _name , decimal _salary , int _age)
+        {
+            Id = _id ;
+            Name = _name ;
+            salary = _salary ;
+            Age = _age;
+        }
+
+        public override string ToString()
+        {
+            return $"ID : {Id} , Name :{Name} , Salary :{Salary} ";
+        }
+    }
+}
--- Assignment2_Session2_OOP/Program.cs
+++ Assignment2_Session2_OOP/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Encapsulation
+            // Separate Date Definition [Attribute] From Its Use [Getter Setter , Property]
+            // Encapsulation With Getter Setter
+            Employee employee = new Employee();
+            employee.SetName("Eman");
+            Console.WriteLine(employee.GetName());
+
+            // Encapsulation With Property
+            employee.Salary = 2000;
+            Console.WriteLine(employee.Salary);
+            employee.Age = 30;
+
+
+
+            #endregion
+
         }
     }
 }
